964c7e499885065b795e34b222c4703eb8e1d13b
Eduardo Pelaez Alvarado <eduardopelaez@pm.me>
2022-09-19 19:40:46 -0700

    feat: verify connection

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,14 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+
+    private
+
+    def find_verified_user
+      if verified_user == env['warden'].user
+        verified_user
+      else
+        reject_unauthorized_connection
+      end
+    end
   end
 end
